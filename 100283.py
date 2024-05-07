11a0b11f3a49d903d546d8337ed1947b17f72a85
Vidya Ganapati <vganapati@gmail.com>
2024-05-06 17:46:14 -0700

    Update 100283.py

--- 100283.py
+++ 100283.py
@@ -1,4 +1,24 @@
 def minAnagramLength(s):
+    def getFactors(k):
+        factors_0 = []
+        factors_1 = []
+        for i in range(1,int(k**0.5)):
+            if k % i == 0:
+                factors_0.append(i)
+                if k//i != i:
+                    factors_1.insert(0,k//i)
+        return factors_0 + factors_1
+    factors = getFactors(len(s))
+
+    position = 0
+    trial_anagram = {s[0]: 1}
+    current_anagram
+    while position < len(s):
+
+
+
+
+def minAnagramLength_0(s):
     find_anagram_len = lambda anagram: sum([trial_anagram[i] for i in trial_anagram.keys()])
     trial_anagram = {} # shortest possible anagram
     current_anagram = {} # current collection
@@ -25,15 +45,18 @@ def minAnagramLength(s):
         if (character in trial_anagram.keys()) and current_anagram[character] <= trial_anagram[character]:
             pass
         else:
-            current_anagram = {}
+            
+            current_anagram = dict(full_anagram)
             trial_anagram = dict(full_anagram)
             trial_anagram_len = find_anagram_len(trial_anagram)
+            current_len = trial_anagram_len
 
 
         if current_len == trial_anagram_len:
             current_anagram = {}
             current_len = 0
-
+    
+    breakpoint()
     return find_anagram_len(trial_anagram)
 
 def test_0():
@@ -45,17 +68,14 @@ def test_1():
     assert minAnagramLength(s) == 4
 
 def test_2():
-    s = "xxe"
-    assert minAnagramLength(s) == 3
-
-def test_3():
-    s = "pqqppqpqpq"
-    assert minAnagramLength(s) == 2
+    s = "aabb"
+    assert minAnagramLength(s) == 4
 
 if __name__ == '__main__':
-    test_3()
+    test_2()
+    breakpoint()
     test_0()
     test_1()
-    test_2()
+    
 
 
