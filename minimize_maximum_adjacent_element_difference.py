6379f319051d2c23028c32b66fd50f40127525da
vganapati <vganapa1@swarthmore.edu>
2024-11-18 22:12:39 -0800

    Update minimize_maximum_adjacent_element_difference.py

--- minimize_maximum_adjacent_element_difference.py
+++ minimize_maximum_adjacent_element_difference.py
@@ -1,77 +1,74 @@
 from typing import List
 
 """
-https://leetcode.com/problems/minimize-the-maximum-adjacent-element-difference/solutions/6055349/java-c-python-no-binary-search-o-1-space/
+https://leetcode.com/problems/minimize-the-maximum-adjacent-element-difference/description/
 """
+
+from typing import List
 
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
         smallest_val = 10**9 + 1
         largest_val = 1
-        pairs = []
-        count = 0
-        min_pair_ind = 0
-        max_pair_ind = 0
-        max_diff = 0
-        for i, num in enumerate(nums):
-            if i<(len(nums)-1):
-                dist = abs(nums[i] - nums[i+1])
-                max_diff = max(max_diff, dist)
-            if num == -1: 
-                if i>0 and i<(len(nums)-1):
-                    left = nums[i-1]
-                    right = nums[i+1]
-                else:
-                    if i==0:
-                        single = nums[i+1]
-                    elif i==len(nums)-1:
-                        single = nums[i-1]
-                    left, right = single,single
-                pairs.append([left,right])
-                if left < smallest_val:
-                    min_pair_ind = count
-                    smallest_val = left
-                if right < smallest_val:
-                    min_pair_ind = count
-                    smallest_val = right
-                if left > largest_val:
-                    max_pair_ind = count
-                    largest_val = left
-                if right > largest_val:
-                    max_pair_ind = count
-                    largest_val = right
-                count += 1
-        breakpoint()
-        if count == 0:
-            return max_diff
-        if min_pair_ind == max_pair_ind:
-            left, right = pairs[min_pair_ind]
-            dist = abs(left-right)
-            return max(max_diff, dist//2 + dist%2)
-        
-        # interval_0 is starting from smallest_val
-        # interval_1 is ending at largest_val
-
-        interval_0_right = smallest_val
-        interval_1_left = largest_val
-
-        for left,right in pairs:
-            min_i = min(left, right)
-            max_i = max(left, right)
-            if abs(smallest_val-max_i) < abs(largest_val-min_i):
-                # place in interval_0
-                interval_0_right = max(interval_0_right, min_i, max_i)
+        maxDiff = 0
+        for i, num in enumerate(nums[:-1]):
+            if nums[i]==-1 or nums[i+1]==-1:
+                if nums[i]>0:
+                    smallest_val = min(smallest_val, nums[i])
+                    largest_val = max(largest_val, nums[i])
+                if nums[i+1]>0:
+                    smallest_val = min(smallest_val, nums[i+1])
+                    largest_val = max(largest_val, nums[i+1])                    
             else:
-                interval_1_left = min(interval_1_left, min_i, max_i)
-        dist_0 = abs(smallest_val-interval_0_right)
-        dist_1 = abs(largest_val-interval_1_left)
-        breakpoint()
-        return max(dist_0//2 + dist_0%2, dist_1//2 + dist_1%2, max_diff)
+                maxDiff = max(maxDiff, 2*abs(nums[i+1]-nums[i]))
+
+        # for i in range(len(nums)):
+            
+
+        pointer_2 = 0
+        while pointer_2<(len(nums)-1) and nums[pointer_2]==-1:
+            pointer_2 += 1
+        
+        if pointer_2 == len(nums)-1:
+            return 0
+
+        if pointer_2 > 0:
+            maxDiff = max(maxDiff,min(abs(smallest_val-nums[pointer_2]), abs(largest_val-nums[pointer_2])))
+        
+        pointer_1 = pointer_2
+        pointer_2 = pointer_2+1
+        two_thirds = ((largest_val - smallest_val + 2)//3)*2 # +2 for ceiling
+        while pointer_2 < len(nums):
+            if nums[pointer_2] == -1:
+                pointer_2 += 1
+            else:
+                pointer_diff = pointer_2 - pointer_1
+                if pointer_diff==1:
+                    pointer_1 += 1
+                    pointer_2 += 1
+                elif pointer_diff==2:
+                    smallest_val_0 = min(nums[pointer_2],nums[pointer_1])
+                    largest_val_0 = max(nums[pointer_2],nums[pointer_1])
+                    maxDiff = max(maxDiff, min(largest_val-smallest_val_0, largest_val_0 - smallest_val))
+                    pointer_1 = pointer_2
+                    pointer_2 += 1
+                elif pointer_diff > 2:
+                    smallest_val_0 = min(nums[pointer_2],nums[pointer_1])
+                    largest_val_0 = max(nums[pointer_2],nums[pointer_1])
+                    maxDiff = max(maxDiff, min(largest_val-smallest_val_0, largest_val_0 - smallest_val, two_thirds))   
+                    pointer_1 = pointer_2
+                    pointer_2 += 1
+        
+        if nums[pointer_2-1] == -1:
+            maxDiff = max(maxDiff, min(abs(smallest_val-nums[pointer_1]), abs(largest_val-nums[pointer_1])))
+        return (maxDiff+1)//2
 
 solution = Solution()
-assert solution.minDifference([29,-1,-1]) == 0
-assert solution.minDifference([14,-1]) == 0
+assert solution.minDifference([-1,-1,-1]) == 0
 assert solution.minDifference([1,12]) == 11
 assert solution.minDifference([1,2,-1,10,8]) == 4
-assert solution.minDifference([-1,-1,-1]) == 0
+assert solution.minDifference([29,-1,-1]) == 0
+assert solution.minDifference([14,-1]) == 0
+
+
 assert solution.minDifference([-1,10,-1,8]) == 1
