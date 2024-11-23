bc87a5605c7d16f6c98d0a1a65c0c45dc70df90c
vganapati <vganapa1@swarthmore.edu>
2024-11-23 00:10:48 -0800

    Create candy.py

---
+++ candy.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        pass
+
+solution = Solution()
+ratings = [1,0,2]
+assert solution.candy(ratings) == 5 
+
+ratings = [1,2,2]
+assert solution.candy(ratings) == 4
