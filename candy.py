89458ec9565bf1d0aa83d83ea892cafcdb2d456c
vganapati <vganapa1@swarthmore.edu>
2024-11-24 13:02:59 -0800

    Update candy.py

--- candy.py
+++ candy.py
@@ -2,7 +2,26 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        pass
+        candy_vec = [1]*len(ratings)
+
+        def forward_pass():
+            for ind, rating in enumerate(ratings):
+                if ind==0:
+                    pass
+                else:
+                    if rating > ratings[ind-1]:
+                        candy_vec[ind] = max(candy_vec[ind], candy_vec[ind-1] + 1)
+        
+        forward_pass()
+
+        # backward pass
+        candy_vec = candy_vec[::-1]
+        ratings = ratings[::-1]
+
+        forward_pass()
+
+        return sum(candy_vec)
+
 
 solution = Solution()
 ratings = [1,0,2]
