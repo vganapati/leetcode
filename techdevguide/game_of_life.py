ce097a1357a087bf5ec44753a34caaecadb3df38
vganapati <vganapa1@swarthmore.edu>
2024-12-19 23:03:05 -0800

    Create game_of_life.py

---
+++ techdevguide/game_of_life.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/problems/game-of-life/
+"""
+
+from typing import List
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead
+        """
+        
